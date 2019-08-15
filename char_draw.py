0c5aa339a3c6b3baa010c325ac1dd52d8605bd1c
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2019-08-16 00:15:33 +0430

    Add files via upload
    Type your favorite character and then type "h" for Heart or "o" for smile :)

---
+++ char_draw.py
@@ -0,0 +1,42 @@
+# This program will draw a heart with the character that user types:
+
+y = input("Type a character you like: ")
+print("h stands for Heart and o stands for Emoji. ")
+x = input("Which Sticker would you like? h or o: ")
+if x == "h":
+    print("                                         ")
+    print("       ",y,y,y,"        ",y,y,y,"         ")
+    print("    ",y,"      ",y,"    ",y,"     ",y,"      ")
+    print("   ",y,"        ",y," ",y,"        ",y,"     ")
+    print("  ",y,"          ",y,y,"          ",y,"    ")
+    print("  ",y,"           ",y,"           ",y,"    ")
+    print("  ",y,"                        ",y,"     ")
+    print("    ",y,"                     ",y,"       ")
+    print("      ",y,"                 ",y,"        ")
+    print("       ",y,"              ",y,"          ")
+    print("         ",y,"           ",y,"           ")
+    print("          ",y,"         ",y,"            ")
+    print("           ",y,"       ",y,"             ")
+    print("            ",y,"     ",y,"             ")
+    print("             ",y,"   ",y,"               ")
+    print("               ", y,y, "                 ")
+    print("                ", y, "                  ")
+
+if x == "o":
+    print("         ",y,y,"          ",y,y,"           ")
+    print("       ",y,"   ",y,"     ",y,"     ",y,"        ")
+    print("     ",y, "      ",y,"  ",y,"         ",y,"      ")
+    print("                                            ")
+    print("",y,"                                    ",y,"")
+    print(" ",y,"                                  ",y,"  ")
+    print("   ",y,"                              ",y,"   ")
+    print("     ",y,"                          ",y,"     ")
+    print("        ",y,y,y,y,y,y,y,y,y,y,y,y,y,"        ")
+
+
+
+
+input("Press Enter to Exit")
+
+
+
